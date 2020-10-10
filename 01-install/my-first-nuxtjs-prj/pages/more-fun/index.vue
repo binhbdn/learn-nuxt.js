07b7ed465acba197e0e33ffc7fc08112f8fa424a
binhbdn <binh.buidn@gmail.com>
2020-10-10 18:06:18 +0700

    Add more-fun/index.vue same as add more-fun.vue

---
+++ 01-install/my-first-nuxtjs-prj/pages/more-fun/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+        <h1>Nuxt.js is more fun!</h1>
+        <p>This page is pre-rendered on the server-side</p>
+        <p>Create a directory named more-fun and put an index.vue file inside. This will give the same result as creating a more-fun.vue file</p>
+  </div>
+</template>
