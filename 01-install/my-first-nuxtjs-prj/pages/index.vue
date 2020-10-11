a79585919ccae2218a93266842bb4ee8e2bfafd4
binhbdn <binh.buidn@gmail.com>
2020-10-11 14:48:28 +0700

    Navigation: add <main>, <NuxtLink>, <a>

--- 01-install/my-first-nuxtjs-prj/pages/index.vue
+++ 01-install/my-first-nuxtjs-prj/pages/index.vue
@@ -1,3 +1,8 @@
 <template>
+<main>
     <h1>Hello NUTX.JS!</h1>
+    <p><NuxtLink to="/fun">Fun page (internal link that belongs to the Nuxt App)</NuxtLink></p>
+    <p><NuxtLink to="/more-fun/">More Fun page (internal link that belongs to the Nuxt App)</NuxtLink></p>
+    <p><a href="https://nuxtjs.org">External Link to another page</a></p>
+</main>
 </template>
