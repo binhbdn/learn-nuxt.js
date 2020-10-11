0b40abfd751d3f8938c312398dde9cdbbc6a88ef
binhbdn <binh.buidn@gmail.com>
2020-10-11 14:47:33 +0700

    Navigation: add <NuxtLink>

--- 01-install/my-first-nuxtjs-prj/pages/more-fun/index.vue
+++ 01-install/my-first-nuxtjs-prj/pages/more-fun/index.vue
@@ -3,5 +3,6 @@
         <h1>Nuxt.js is more fun!</h1>
         <p>This page is pre-rendered on the server-side</p>
         <p>Create a directory named more-fun and put an index.vue file inside. This will give the same result as creating a more-fun.vue file</p>
+        <p><NuxtLink to="/">Home page</NuxtLink></p>
   </div>
 </template>
